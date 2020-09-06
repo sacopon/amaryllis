a2132c61ed6d2d88713050c8722283a89cf8735d
Takeshi Sakurai <sacopon.dev@gmail.com>
2020-09-06 13:18:39 +0900

    :sparkles: テスト用のシーン作成

---
+++ src/presentation/scenes/base_scene.ts
@@ -0,0 +1,35 @@
+import { Container, Application } from "pixi.js";
+import { screen } from "presentation/application/config/configuration";
+import { ServiceLocator } from "presentation/application/service_locator";
+
+export interface Scene {
+  initialize(): void;
+  update(): void;
+  fetchStaticResourcesAsync(): Promise<void | Error>;
+}
+
+export class BaseScene extends Container implements Scene {
+  private application: Application;
+
+  public constructor() {
+    super();
+
+    this.application = ServiceLocator.getApplication();
+    this.position.set(Math.floor(screen.resolution.width / 2), Math.floor(screen.resolution.height / 2));
+  }
+
+  public initialize() {}
+
+  public update() {}
+
+  public fetchStaticResourcesAsync() {
+    return Promise.resolve();
+  }
+
+  /**
+   * @protected
+   */
+  public get _app() {
+    return this.application;
+  }
+}
